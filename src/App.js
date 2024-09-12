bf683f421e8ad3176c62cb1b8d04a1248e79420c
sarthak <thesarthakchauhan@gmail.com>
2024-09-12 16:37:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from 'react';
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [flag, setFlag] = useState(0);
+
+  // useMemo will memoize the result of this expensive calculation
+  const expensiveCalculation = useMemo(() => {
+
+    console.log('Expensive calculation running...');
+    for (let i = 0; i < 1000000000; i++) {}
+  }, [flag]);// run when flag changes 
+   // Empty dependency array, so this function only runs once, when the component mounts
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Expensive Calculation with useMemo</h1>
+      <p>Expensive calculation result: {expensiveCalculation}</p>
+      <p>Count: {count}</p>
+      
+      {/* Button to update the count state */}
+      <button onClick={() => {
+        setCount(count + 1)
+        count===10? setFlag(1):setFlag(0)
+        }}>Increase Count</button>
     </div>
   );
 }
